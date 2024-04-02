458e7efb9e5f3f4a7405c149aa042fdbd102b541
ManuelHeraz <hernandezdiazjm00@gmail.com>
2024-04-02 17:05:52 -0600

    v24.04.02.0505

---
+++ assets/js/music-list.js
@@ -0,0 +1,299 @@
+let allMusic = [
+    
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse1" //name here
+    },
+
+    {
+        name: "CLUBZ - ÁFRIKA",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse2-Afrika" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse3" //name here
+    },
+
+    {
+        name: "Hot Chip - Straight To The Morning ft. Jarvis Cocker",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse4-StraightToTheMorning" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse5" //name here
+    },
+
+    {
+        name: "Diamante Eléctrico - Suéltame, Bogotá",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse6-Sueltame" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse7" //name here
+    },
+
+    {
+        name: "Jungle - All Of The Time",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse8-AllOfTheTime" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse9" //name here
+    },
+
+    {
+        name: "De Lux - Cool Up",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse10-CoolUp" //name here
+    },
+
+    {
+        name: "Kid Moxie, LUXXURY - All I Want",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse11-AllIWant" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse12" //name here
+    },
+
+    {
+        name: "Gorillaz - The Valley of The Pagans",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse13-Gorillaz" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse14" //name here
+    },
+
+    {
+        name: "Lucky Chops - Full Heart",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse15-FullHeart" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse16" //name here
+    },
+
+    {
+        name: "Autor - Feel So Good (?)",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse17-Feel" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse18" //name here
+    },
+
+    {
+        name: "Dua Lipa - Levitating",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse19-Levitating" //name here
+    },
+
+    {
+        name: "Classixx & Local Natives - Weekends",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse21-Weekends" //name here
+    },
+
+    {
+        name: "ODESZA, The Chamanas - Everything At Your Feet",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse22-EverythingAtYourFeet" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse23" //name here
+    },
+
+    {
+        name: "Porter Robinson - Look at the Sky",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse24-LookattheSky" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse25" //name here
+    },
+
+    {
+        name: "Centavrvs - El Punto Final",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse26-PuntoFinal" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse27" //name here
+    },
+
+    {
+        name: "Remi Wolf - Hello Hello Hello",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse28-Hello" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse29" //name here
+    },
+
+    {
+        name: "PRXZM - Where We Started",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse30-Started" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse31" //name here
+    },
+
+    {
+        name: "Sad Alex - New Hearthbreak",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse32-Hearthbreak" //name here
+    },
+
+    {
+        name: "SAINT MOTEL - Preach",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse33-Preach" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse34" //name here
+    },
+
+    {
+        name: "SOTO MAYOR - Quema",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse35-Quema" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse36" //name here
+    },
+
+    
+    {
+        name: "OTR - Midnight Sun",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse37-Midnight" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse38" //name here
+    },
+
+    {
+        name: "Whethan - Sunshine",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse39-Sunshine" //name here
+    },
+
+    
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse40" //name here
+    },
+
+    {
+        name: "Young & Sick - OHH My Ghosts",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse41-OHHMyGhosts" //name here
+    },
+
+    {
+        name: "Horizon Pulse",//music name
+        artist: "Forza Horizon 5", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse42" //name here
+    },
+
+    {
+        name: "Glass Animals - Heat Waves (Shakur Ahmad Remix)",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "HorizonPulse43-HeatWaves" //name here
+    },
+
+];
